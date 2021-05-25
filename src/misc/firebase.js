7b87ba94b3cc85beaab9ecde89b6c1d99cb4e741
Aashrith001 <v.aashrith22@gmail.com>
2021-05-25 15:58:47 +0530

    initialised firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+const config = {
+  apiKey: "AIzaSyAu2VnkLiT41CdRi_wHVW__41-CmXs8UJI",
+  authDomain: "candor-28a5a.firebaseapp.com",
+  databaseURL: "https://candor-28a5a-default-rtdb.firebaseio.com",
+  projectId: "candor-28a5a",
+  storageBucket: "candor-28a5a.appspot.com",
+  messagingSenderId: "344379004886",
+  appId: "1:344379004886:web:e7cc1f9b1598eee0f9f677"
+};
+
+firebase.initializeApp(config);
+
