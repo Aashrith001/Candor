a28a9fcc39dcb51243ababab15d2093a2e238f12
Aashrith001 <v.aashrith22@gmail.com>
2021-06-12 09:53:52 +0530

    managed user with user context

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import 'rsuite/dist/styles/rsuite-default.css';
 import './styles/main.scss';
 import {Switch} from 'react-router';
@@ -7,17 +6,23 @@ import SignIn from './pages/SignIn';
 import PrivateRoute from './components/PrivateRoute';
 import PublicRoute from './components/PublicRoute';
 import Home from './pages/Home';
+import { ProfileProvider } from './context/profile.context';
 
 function App() {
   return (
-    <Switch>
-      <PublicRoute path="/signin">
-        <SignIn />
-      </PublicRoute>
-      <PrivateRoute path="/">
-        <Home />
-      </PrivateRoute>
-    </Switch>
+    <ProfileProvider>
+      <Switch>
+      <PublicRoute path="/hello">
+          heloo sajhfkj
+        </PublicRoute>
+        <PublicRoute path="/signin">
+          <SignIn />
+        </PublicRoute>
+        <PrivateRoute path="/">
+          <Home />
+        </PrivateRoute>
+      </Switch>
+    </ProfileProvider>
   );
 }
 
