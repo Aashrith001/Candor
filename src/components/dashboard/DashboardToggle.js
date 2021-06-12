c5a456dbff5909eaf68273e7e64cb72f52f5be52
Aashrith001 <v.aashrith22@gmail.com>
2021-06-12 10:57:18 +0530

    builded sidebar component

---
+++ src/components/dashboard/DashboardToggle.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Button,Icon ,Drawer} from 'rsuite'
+import { useMediaQuery, useModelState } from '../../misc/custom-hooks'
+import Dashboard from '.';
+
+const DashboardToggle = () => {
+  const {isOpen,close,open} = useModelState();
+  const isMobile = useMediaQuery('(max-width: 992px)');
+
+  return (
+    <>
+      <Button block color="blue" onClick={open}>
+          <Icon icon="dashboard"/> Dashboard
+      </Button>
+      <Drawer full={isMobile} show={isOpen} onHide={close} placement="left">
+        <Dashboard />
+      </Drawer>
+    </>
+  ) 
+}
+
+export default DashboardToggle
