9f5b2e56932ccbaf32aa401e9ee1da089eea7a52
Aashrith001 <v.aashrith22@gmail.com>
2021-06-13 23:15:14 +0530

    displayed avatar on dashboard

--- src/components/dashboard/AvatarUploadBtn.js
+++ src/components/dashboard/AvatarUploadBtn.js
@@ -4,6 +4,7 @@ import AvatarEditor from 'react-avatar-editor'
 import { database, storage } from '../../misc/firebase'
 import { useModelState } from '../../misc/custom-hooks'
 import { useProfile } from '../../context/profile.context'
+import ProfileAvatar from '../ProfileAvatar'
 
 const fileInputTypes = ".png .jpeg, .jpg"
 const acceptedFileTypes = ['image/png', 'image/jpeg', 'image/pjpeg']
@@ -66,9 +67,14 @@ const AvatarUploadBtn = () => {
       Alert.error(err.message, 4000);
     }
   }
-
+  console.log(profile);
   return (
     <div className="mt-3 text-center">
+      <ProfileAvatar
+        src={profile.avatar}
+        name={profile.name}
+        className="width-200 height-200 img-fullsize font-huge"
+      />
       <div>
         <label
           htmlFor="avatar-upload"
